75319e4f09c7a1c9165ddeb3202aa990cfbd38b4
MichelleF <54815746+MichelleFdo@users.noreply.github.com>
2021-12-30 01:13:55 +0530

    Update urlConfig.js

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,4 @@
-export const backendUrl = "https://testauth.boligmappa.no/auth/realms/professional-realm-staging/protocol/openid-connect";
+export const backendUrl = "https://testauth.boligmappa.no/auth/realms/professional-realm-staging/protocol/openid-connect/";
 export const authUrl = "https://testauth.boligmappa.no/auth/realms/professional-realm-staging/protocol/openid-connect/auth";
 export const api = "https://staging-proff-api.boligmappa.no/v1/"
 // export const ngrokUrl = 'https://62cf-2402-4000-13c6-f0ec-a567-3bf4-ad30-2749.ngrok.io/api';
