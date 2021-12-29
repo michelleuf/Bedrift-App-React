3b9f958f7cd017edd4bd010b869535c846a2fefd
MichelleF <54815746+MichelleFdo@users.noreply.github.com>
2021-12-30 00:27:57 +0530

    Update Authentication.js

--- src/containers/Authentication.js
+++ src/containers/Authentication.js
@@ -6,58 +6,60 @@ export default function Authentication() {
   const getAuthCode = () => {
     let search = window.location.search;
     let params = new URLSearchParams(search);
-    let codee = params.get('code');
-    
-    setData(codee);
-
-    sessionStorage.setItem('code', codee);
+    let code = params.get('code');
+    sessionStorage.setItem('code', code);
+    setData(code);
   };
     
   //get access token
   const getToken =() =>{
     const authCode = data;
     console.log(authCode);
+
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-    myHeaders.append("Access-Control-Allow-Origin", 'no-cors');
-        
     var urlencoded = new URLSearchParams();
-    urlencoded.append("client_id",process.env.REACT_APP_CLIENT_ID);
+    urlencoded.append("client_id", process.env.REACT_APP_CLIENT_ID);
     urlencoded.append("client_secret", process.env.REACT_APP_CLIENT_SECRET);
-    urlencoded.append("username",process.env.REACT_APP_USERNAME);
-    urlencoded.append("password",process.env.REACT_APP_PASSWORD);
-    urlencoded.append("grant_type",'password' );
-    // urlencoded.append("grant_type",'authorization_code' );
-    // urlencoded.append("code", authCode);
-    // urlencoded.append("response_type",'code' );
-    urlencoded.append("redirect_uri", "/plants");
+    urlencoded.append("grant_type", "authorization_code");
+    urlencoded.append("code", authCode);
+    urlencoded.append("redirect_uri", "http://localhost:3000/redirect");
     urlencoded.append("scope", "openid");
 
+    // urlencoded.append("username",process.env.REACT_APP_USERNAME);
+    // urlencoded.append("password",process.env.REACT_APP_PASSWORD);
+    // urlencoded.append("grant_type",'password' );
+    
     var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: urlencoded,
-    redirect: 'follow'
+      method: 'POST',
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: 'follow'
     };
 
-    fetch('token', requestOptions).then(response => response.json()).then(result => {
-        console.log('dataaaaaaaa',result);
-        // sessionStorage.setItem('authToken', data.access_token);
-    }).catch(error => console.log('error', error));        
+    fetch('/token', requestOptions).then(async response => {
+        try {
+          const data1 = await response.json();
+          console.log('respose data ?',data1);
+          sessionStorage.setItem('access_token', data1['access_token'])
+        } catch (error) {
+          console.log('error happened here!!!!')
+          console.log(error)
+        }
+      })
   }
 
     useEffect(()=>{
         getAuthCode();
-        getToken();
     },[]); // eslint-disable-line react-hooks/exhaustive-deps
 
-    // useEffect(()=>{
-    //   if (data !== "") getToken();
-    // },[data]); // eslint-disable-line react-hooks/exhaustive-deps
+    useEffect(()=>{
+      if (data !== "") getToken();
+    },[data]); // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
         <div>
-          Please wait
+          Access Token generating...
         </div>
     )
 }
