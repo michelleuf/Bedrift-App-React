1c5cc4d5a2365aa4466432450cf6467f6846d130
MichelleF <54815746+MichelleFdo@users.noreply.github.com>
2022-01-06 14:43:29 +0530

    load profession type, document type, room type from APIs

--- src/containers/bedrift/Documents.js
+++ src/containers/bedrift/Documents.js
@@ -18,6 +18,7 @@ import DialogActions from '@mui/material/DialogActions';
 import PhotoCamera from '@mui/icons-material/PhotoCamera';
 import TextField from '@mui/material/TextField';
 import Button from "../../components/Dashboard/CustomButtons/Button";
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 export default function Documents(props) {
     const boligmappaNumber = props.boligmappaNumber;
@@ -32,6 +33,7 @@ export default function Documents(props) {
         setOpen(false);
     }; 
 
+    //create document
     const [fileId, setFileId] = React.useState(null);
     const [fileName, setFileName] = React.useState("");
     const [title, setTitle] = React.useState("");
@@ -52,6 +54,8 @@ export default function Documents(props) {
     const [roomTypeId, setRoomTypeId] = React.useState(null);
     const [roomType, setRoomType] = React.useState("");
     const [roomDescription, setRoomDescription] = React.useState("");
+    const [error, setError] = React.useState("");
+
     const uploadFile = () => {
         const token = window.localStorage.getItem('token');
         axios.post(`${api}plants/${boligmappaNumber}/files`,
@@ -96,9 +100,13 @@ export default function Documents(props) {
           .then(res =>{
             const results =  res.data.response;
             console.log(results);
-        });
+        }).catch(error =>{
+            console.log(error.response.data.message.en);
+            setError(error.response.data.message.en);
+          });
     }
 
+    // get existing files
     const [data, setData] = React.useState();
     const getdata =() =>{
         const token = window.localStorage.getItem('token');
@@ -114,8 +122,7 @@ export default function Documents(props) {
             setData(results);               
         });
       }
-      
-    const Columns = [
+      const Columns = [
         { id: 'FileName', label: 'fileName'},
         { id: 'title', label: 'title'},
         { id: 'description', label: 'description'},
@@ -124,12 +131,124 @@ export default function Documents(props) {
 
     ];
 
+
+    // get room types
+    const [roomTypes, setRoomTypes] = React.useState([]);
+    const getRoomTypes =() =>{
+        const token = window.localStorage.getItem('token');
+        axios.get(`${api}types/roomTypes`,{
+          headers: {
+            'Authorization': token ? `Bearer ${token}` : '',
+            'Access-Control-Allow-Origin': '*',
+          }
+          })
+          .then(res =>{
+            const results =  res.data.response;
+            console.log(results);
+            setRoomTypes(results);               
+        }).catch(error =>{
+          console.log(error);
+        });
+      }
+  const handlechangeR = (event, value) => {
+      setRoomTypeId(value.id);
+      setRoomType(value.name);
+  }
+  const optionsR = roomTypes.map((option) => {
+      const firstLetter = option.name[0].toUpperCase();
+      return {
+          firstLetter: /[0-9]/.test(firstLetter) ? '0-9' : firstLetter,
+          ...option,
+      };
+  });
+
+  // get document types
+  const [documentTypes, setDocumentTypes] = React.useState([]);
+  const getDocumentTypes =() =>{
+      const token = window.localStorage.getItem('token');
+      axios.get(`${api}types/documentTypes`,{
+        headers: {
+          'Authorization': token ? `Bearer ${token}` : '',
+          'Access-Control-Allow-Origin': '*',
+        }
+        })
+        .then(res =>{
+          const results =  res.data.response;
+          console.log(results);
+          setDocumentTypes(results);               
+      }).catch(error =>{
+        console.log(error);
+      });
+    }
+    const handlechangeD = (event, value) => {
+        setDocumentTypeId(value.id);
+        setDocumentTypeName(value.name);
+    }
+    const optionsD = documentTypes.map((option) => {
+        const firstLetter = option.name[0].toUpperCase();
+        return {
+            firstLetter: /[0-9]/.test(firstLetter) ? '0-9' : firstLetter,
+            ...option,
+        };
+    });
+
+    // get profession types
+  const [professiomTypes, setProfessiomTypes] = React.useState([]);
+  const getProfessionTypes =() =>{
+      const token = window.localStorage.getItem('token');
+      axios.get(`${api}types/professionTypes`,{
+        headers: {
+          'Authorization': token ? `Bearer ${token}` : '',
+          'Access-Control-Allow-Origin': '*',
+        }
+        })
+        .then(res =>{
+          const results =  res.data.response;
+          console.log(results);
+          setProfessiomTypes(results);               
+      }).catch(error =>{
+        console.log(error);
+      });
+    }
+    const handlechangeP = (event, value) => {
+        setProfessionTypeId(value.id);
+        setProfessionTypeName(value.name);
+    }
+    const optionsP = professiomTypes.map((option) => {
+        const firstLetter = option.name[0].toUpperCase();
+        return {
+            firstLetter: /[0-9]/.test(firstLetter) ? '0-9' : firstLetter,
+            ...option,
+        };
+    });
+      
+    
     React.useEffect(()=>{
         getdata();
-      },[]);
+        getRoomTypes();
+        getDocumentTypes();
+        getProfessionTypes();
+
+      },[]);  // eslint-disable-line react-hooks/exhaustive-deps
       
     return (
         <div>
+            <Box
+            component="form"
+            noValidate
+            sx={{
+              '& .MuiTextField-root': { m: 1, width: '90%'},
+            }}
+            autoComplete="off"
+            border={0}
+            padding={1}
+            margin={2}
+            align={'right'}
+            >
+                    <Button variant="contained" component="span" onClick={handleClickOpen}>          
+                        <PhotoCamera />Upload File
+                    </Button>
+            </Box>
             <div>
                 <FormControl fullWidth variant="outlined" size="small">
                 <OutlinedInput
@@ -207,33 +326,18 @@ export default function Documents(props) {
                 </TableBody>
                 </Table>
             </TableScrollbar>
-            <Box
-            component="form"
-            noValidate
-            sx={{
-              '& .MuiTextField-root': { m: 1, width: '90%' },
-            }}
-            autoComplete="off"
-            border={0}
-            padding={1}
-            marginTop={5}
-            margin={2}
-            >
-                    <Button variant="contained" component="span" onClick={handleClickOpen}>          
-                        <PhotoCamera />Upload File
-                    </Button>
-            </Box>
 
             <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
                 <DialogTitle id="customized-dialog-title" onClose={handleClose}>
                 Upload File
+                <p style={{color: "red",fontSize:'12px'}}>{error}</p>
                 </DialogTitle>
                 <DialogContent dividers>
                     <Box
                         component="form"
                         noValidate
                         sx={{
-                        '& .MuiTextField-root': { m: 1, width: '90%' },
+                        '& .MuiTextField-root': { m: 1, width: '90%', padding: '5px'},
                         }}
                         autoComplete="on"
                         padding={1}
@@ -249,20 +353,42 @@ export default function Documents(props) {
                         <TextField id="outlined-size-small" label="tagId" fullWidth size="small" onChange={(e)=>setTagId(e.target.value)}/>
                         <TextField id="outlined-size-small" label="tagName" fullWidth size="small" onChange={(e)=>setChapterName(e.target.value)}/>
                         <TextField id="outlined-size-small" label="tagDescription" fullWidth size="small" onChange={(e)=>setTagDescription(e.target.value)}/>
-                        <TextField id="outlined-size-small" label="professionTypeId" fullWidth size="small" onChange={(e)=>setProfessionTypeId(e.target.value)}/>
-                        <TextField id="outlined-size-small" label="professionTypeName" fullWidth size="small" onChange={(e)=>setProfessionTypeName(e.target.value)}/>
-                        <TextField id="outlined-size-small" label="documentTypeId" fullWidth size="small" onChange={(e)=>setDocumentTypeId(e.target.value)}/>
-                        <TextField id="outlined-size-small" label="documentTypeName" fullWidth size="small" onChange={(e)=>setDocumentTypeName(e.target.value)}/>
+                        <Autocomplete
+                                disableClearable
+                                options={optionsP.sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter))}
+                                groupBy={(option) => option.firstLetter}
+                                getOptionLabel={(option) => option.id + " - " + option.name}
+                                renderInput={(params) => <TextField {...params} label="Profession Type" required />}
+                                size="small"
+                                onChange={(event, value) => handlechangeP(event, value)}
+                            />
+                        <Autocomplete
+                                disableClearable
+                                options={optionsD.sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter))}
+                                groupBy={(option) => option.firstLetter}
+                                getOptionLabel={(option) => option.id + " - " + option.name}
+                                renderInput={(params) => <TextField {...params} label="Document Type" required />}
+                                size="small"
+                                onChange={(event, value) => handlechangeD(event, value)}
+                            />
                         <TextField id="outlined-size-small" label="roomId" fullWidth size="small" onChange={(e)=>setRoomId(e.target.value)}/>
                         <TextField id="outlined-size-small" label="roomTitle" fullWidth size="small" onChange={(e)=>setRoomTitle(e.target.value)}/>
-                        <TextField id="outlined-size-small" label="roomTypeId" fullWidth size="small" onChange={(e)=>setRoomTypeId(e.target.value)}/>
-                        <TextField id="outlined-size-small" label="roomType" fullWidth size="small" onChange={(e)=>setRoomType(e.target.value)}/>
+                        <Autocomplete
+                                disableClearable
+                                options={optionsR.sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter))}
+                                groupBy={(option) => option.firstLetter}
+                                getOptionLabel={(option) => option.id + " - " + option.name}
+                                renderInput={(params) => <TextField {...params} label="Room Type" required />}
+                                size="small"
+                                onChange={(event, value) => handlechangeR(event, value)}
+                            />
                         <TextField id="outlined-size-small" label="roomDescription" fullWidth size="small" onChange={(e)=>setRoomDescription(e.target.value)}/>
 
                         <br/>
                             <input type="checkbox" defaultChecked={checked} onChange={() => setChecked(!checked)} />
                             Show in Boligmap
                     </Box>
+
                 </DialogContent>
                 <DialogActions>
                 <Button autoFocus onClick={uploadFile} color="primary" size="sm">
