87c47e1e1068daaaa72542ba4592a40dbde953af
MichelleF <54815746+MichelleFdo@users.noreply.github.com>
2022-01-19 12:33:43 +0530

    miscellaneous

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-// import authReducers from "./auth.reducers";
-// import { combineReducers } from "redux";
-
-// const rootReducer = combineReducers({
-//       auth: authReducers
-// })
-
-// export default rootReducer;
